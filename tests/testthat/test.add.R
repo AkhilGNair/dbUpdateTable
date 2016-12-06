356dc8c56ca31c9e6282bd5d0e1396accd447821
AkhilNairAmey <Akhil.nair@amey.co.uk>
2016-12-06 10:35:29 +0000

    added tests for add

--- tests/testthat/test.add.R
+++ tests/testthat/test.add.R
@@ -1,3 +1,5 @@
+# library(magrittr)
+
 model_People = data.table::data.table(
   PersonID = integer(0),
   LastName = character(0),
@@ -10,8 +12,47 @@ dt_people = data.table::copy(model_People)
 
 # dt_people = dt_people %>% dbCreateTable::add(1, "LastName1", "Akhil")
 
-one_all_unnamed_ordered = dt_people %>% dbCreateTable::add(2, "LastName3", "Chris",  65)
-one_all_named_unordered = dt_people %>% dbCreateTable::add(PersonID = 3, FirstName = "Melody", LastName = "LastName4", Age = 26)
-one_few_named           = dt_people %>% dbCreateTable::add(PersonID = 4, LastName = "LastName5", Age = 21)
-mult_few_named_list     = dt_people %>% dbCreateTable::add(list(PersonID = c(1, 7), LastName = c("LastName1", "LastName7")))
-one_unnamed_list        = dt_people %>% dbCreateTable::add(list(6, "LastName6", "Tom", 30))
+# Add one, all unnamed variables, ordered
+test1 = dt_people %>% dbCreateTable::add(2, "LastName3", "Chris",  65)
+expected_test1 = data.table::data.table(PersonID = 2, LastName = "LastName3", FirstName = "Chris", Age = 65)
+
+# Add one, all named variables, unordered
+test2 = dt_people %>% dbCreateTable::add(PersonID = 3, FirstName = "Melody", LastName = "LastName4", Age = 26)
+expected_test2 = data.table::data.table(PersonID = 3, LastName = "LastName4", FirstName = "Melody", Age = 26)
+
+# Add one, not all variables, names
+test3 = dt_people %>% dbCreateTable::add(PersonID = 4, LastName = "LastName5", Age = 21)
+expected_test3 = data.table::data.table(PersonID = 4, LastName = "LastName5", FirstName = NA_character_, Age = 21)
+
+# Add one by list, all variables, unnamed
+test4 = dt_people %>% dbCreateTable::add(list(6, "LastName6", "Tom", 30))
+expected_test4 = data.table::data.table(PersonID = 6, LastName = "LastName6", FirstName = "Tom", Age = 30)
+
+# Add multiple, not all variables, named
+test5 = dt_people %>% dbCreateTable::add(list(PersonID = c(1, 7), LastName = c("LastName1", "LastName7")))
+expected_test5  = data.table::data.table(PersonID = c(1, 7),
+                                         LastName = c("LastName1", "LastName7"),
+                                         FirstName = c(NA_character_, NA_character_),
+                                         Age = c(NA_real_, NA_real_))
+
+context("Table inserts work correctly")
+test_that("Can add ordered unnamed variables", {
+  expect_equal(test1, expected_test1)
+})
+
+test_that("Can add unordered named variables", {
+  expect_equal(test2, expected_test2)
+})
+
+test_that("Can named variables when not all are present", {
+  expect_equal(test3, expected_test3)
+})
+
+test_that("Can add ordered variables via a list", {
+  expect_equal(test4, expected_test4)
+})
+
+test_that("Can add multiple, named, unordered variables via a list", {
+  expect_equal(test5, expected_test5)
+})
+
